3060731d9d4c23ee4f0ae7b4a9ac3a9bce006e48
liuYang <aittbb@sina.com>
2019-09-09 10:41:12 +0800

    最后一次了,不写了

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="login">
     我是login组件
-    <el-button type="success" plain circle>成功的按钮</el-button>
+    <el-button type="success" plain circle @click= 'toHome' >成功的按钮</el-button>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    toHome () {
+      this.$router.push('/home')
+    }
+  }
 }
 </script>
 
